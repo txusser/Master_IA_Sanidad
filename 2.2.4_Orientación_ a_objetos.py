38bf2bc8529811606a87772cd21209e4b58081e3
jpabloglez <juanpablogglez@gmail.com>
2021-10-22 16:41:50 +0200

    Create 2.2.4_Orientación_ a_objetos.py

---
+++ 2.2.4_Orientación_ a_objetos.py
@@ -0,0 +1,8 @@
+# 2.2.4 Orientación a objetos
+# En este módulo realizaremos algunos ejercicios para demostrar nuestro s conocimientos de XXX
+
+# Creamos nuestra primera clase 'Poligono'
+class Poligono(objet):
+  {
+  
+  }
