45ae88a8117c5234d5204cc28fb4f29959024ba5
jpabloqubio <juanpablo@qubiotech.com>
2021-10-22 18:02:55 +0200

    Primeros ejemplos prácticos Temas: 2.2.4 y 2.3.3

--- Modulo_2/2.2.4_Orientación_a_objetos.py
+++
@@ -1,8 +0,0 @@
-# 2.2.4 Orientación a objetos
-# En este módulo realizaremos algunos ejercicios para demostrar nuestro s conocimientos de XXX
-
-# Creamos nuestra primera clase 'Poligono'
-class Poligono(objet):
-  {
-  
-  }
